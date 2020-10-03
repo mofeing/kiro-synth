befd94958c13bae5131b7908215b8941e3549fe8
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-10-03 20:37:53 +0200

    Refactor DelayLine
    - DelayLine buffer is allocated externally
    - Delay is now the responsible for the time - discrete samples conversion

--- kiro-synth-core/src/effects/delay.rs
+++ kiro-synth-core/src/effects/delay.rs
@@ -1,55 +1,60 @@
 use crate::float::Float;
-use std::collections::VecDeque;
 
-struct DelayLine<F: Float> {
-  sample_rate: F,
-  buffer: VecDeque<F>,
+struct DelayLine<'a, F: Float> {
+  head: usize,
+  buffer: &'a mut [F],
 }
 
-impl<F: Float> DelayLine<F> {
-  pub fn new(sample_rate: F) -> Self {
-    let delay_max = F::val(2.0);
-    let len = (sample_rate * delay_max).ceil().to_usize().unwrap();
-    Self {
-      sample_rate,
-      buffer: VecDeque::<F>::with_capacity(len),
-    }
+impl<'a, F: Float> DelayLine<'a, F> {
+  pub fn new(buffer: &'a mut [F]) -> Self {
+    Self { head: 0, buffer }
   }
 
   pub fn update(&mut self, input: F) {
-    self.buffer.pop_front();
-    self.buffer.push_back(input);
+    self.buffer[self.head] = input;
+    self.head = (self.head + 1) % self.buffer.len();
   }
 
-  pub fn get(&self, index: F) -> F {
-    let index = (index * self.sample_rate).to_usize().unwrap();
-    *self.buffer.iter().nth_back(index).unwrap_or(&F::zero())
+  pub fn get(&self, delay_samples: usize) -> F {
+    let offset = self.buffer.len().min(delay_samples);
+
+    let index = if offset > self.head {
+      self.buffer.len() - offset + self.head
+    } else {
+      self.head - offset
+    };
+    self.buffer[index]
   }
 }
 
-pub struct Delay<F: Float> {
-  delay: F,
+pub struct Delay<'a, F: Float> {
+  delay_samples: usize,
+  delay_seconds: F,
   feedback: F,
   mix: F,
-  delayline: DelayLine<F>,
+  delayline: DelayLine<'a, F>,
+  sample_rate: F,
 }
 
-impl<F: Float> Delay<F> {
-  pub fn new(sample_rate: F) -> Self {
+impl<'a, F: Float> Delay<'a, F> {
+  pub fn new(sample_rate: F, buffer: &'a mut [F]) -> Self {
     Self {
-      delay: F::one(),     // delay in seconds NOTE it must never be 0
+      delay_samples: 1, // delay in samples NOTE it must never be 0
+      delay_seconds: F::val(1) / sample_rate,
       feedback: F::zero(), // between 0 and 1
       mix: F::zero(),      // between 0 and 1
-      delayline: DelayLine::<F>::new(sample_rate),
+      delayline: DelayLine::<F>::new(buffer),
+      sample_rate,
     }
   }
 
-  pub fn set_delay(&mut self, delay: F) {
-    self.delay = delay
+  pub fn set_delay_seconds(&mut self, delay_seconds: F) {
+    self.delay_seconds = delay_seconds;
+    self.delay_samples = (delay_seconds * self.sample_rate).to_usize().unwrap()
   }
 
-  pub fn get_delay(&self) -> F {
-    self.delay
+  pub fn get_delay_seconds(&self) -> F {
+    self.delay_seconds
   }
 
   pub fn set_feedback(&mut self, feedback: F) {
@@ -69,7 +74,7 @@ impl<F: Float> Delay<F> {
   }
 
   pub fn process(&mut self, input: F) -> F {
-    let sample = self.delayline.get(self.delay);
+    let sample = self.delayline.get(self.delay_samples);
     self.delayline.update(input + sample * self.feedback);
 
     sample * self.mix + input * (F::val(1.0) - self.mix)
